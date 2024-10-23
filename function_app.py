e231e99b413e0ca2b452139f70da5aa01e05a800
omararra <omarsamirelmokadem@gmail.com>
2024-10-23 18:47:00 +0300

    updated function_app

--- function_app.py
+++ function_app.py
@@ -1,4 +1,5 @@
 import logging
+import main
 import azure.functions as func
 
 app = func.FunctionApp()
@@ -6,6 +7,7 @@ app = func.FunctionApp()
 @app.schedule(schedule="* * * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
+    main.main()
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
